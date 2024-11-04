2c574458b6bc9e9eeeca95cc28450ce83d43dae2
Fabiola-qa-tester <fabiola.qa.tester@gmail.com>
2024-11-04 10:57:52 -0300

    Fazendo upload de arquivos com Cypress

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -113,15 +113,33 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.get('.button').click()
         cy.get('.error').should('be.visible')
     })
-    it.only('seleciona um arquivo da pasta fixtures', () => {
+    it('seleciona um arquivo da pasta fixtures', () => {
         cy.get('input[type="file"]')
             .should('not.have.value')
             .selectFile('./cypress/fixtures/example.json')
             .should(($input) => {
                 // console.log($input)
                 expect($input[0].files[0].name).to.equal('example.json')
-            })      
-            
+            })
+
+    })
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.get('input[type="file"]')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+            .should(($input) => {
+                // console.log($input)
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+            .selectFile('@sampleFile')
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+
     })
 
 })
