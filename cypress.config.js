d27d8c7bc59db35752c073d330ea1777b83dafae
Fabiola Ângelo <Sfabiola012angelo@gmail.com>
2025-02-13 10:58:14 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  projectId: "5z3wo6",
   e2e: {
     // video: true
   },
+
 })
