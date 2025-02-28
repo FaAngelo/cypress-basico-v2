984d772f18ed0a16d0a943e3a94b08a593547886
Fabiola Ângelo <Sfabiola012angelo@gmail.com>
2025-02-28 11:10:14 -0300

    Fazendo uso do lodash

--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -3,8 +3,11 @@ describe('testes a página da política de privacidade ', () => {
     beforeEach(() => {
         cy.visit('src/privacy.html')
     })
-    it('testa a página da política de privacidade de forma independente', () => {
-        cy.contains('h1', 'CAC TAT - Política de privacidade').should('be.visible')
-        cy.contains('p', 'Talking About Testing').should('be.visible')
+    Cypress._.times(3,()=>{
+        it('testa a página da política de privacidade de forma independente', () => {
+            cy.contains('h1', 'CAC TAT - Política de privacidade').should('be.visible')
+            cy.contains('p', 'Talking About Testing').should('be.visible')
+        })
     })
+   
 })
